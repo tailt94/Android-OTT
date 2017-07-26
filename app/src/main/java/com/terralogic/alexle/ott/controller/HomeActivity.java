5b455163db36e2e2407779259c6cb39f47583fab
Alex le <alex.le@terralogic.com>
2017-07-26 15:17:04 +0700

    Add Home screen layout

--- app/src/main/java/com/terralogic/alexle/ott/controller/HomeActivity.java
+++ app/src/main/java/com/terralogic/alexle/ott/controller/HomeActivity.java
@@ -1,5 +1,11 @@
 package com.terralogic.alexle.ott.controller;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -8,22 +14,72 @@ import com.terralogic.alexle.ott.R;
 import com.terralogic.alexle.ott.model.Device;
 import com.terralogic.alexle.ott.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
-    private TextView textContent;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         bindViews();
-        showUserInfo();
+        setupActionBar();
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void bindViews() {
-        textContent = (TextView) findViewById(R.id.content);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
     }
 
-    private void showUserInfo() {
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setElevation(0);
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new DialFragment(), "DIAL");
+        adapter.addFragment(new CallLogsFragment(), "CALLLOGS");
+        adapter.addFragment(new SettingsFragment(), "SETTINGS");
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private List<Fragment> fragmentList = new ArrayList<>();
+        private List<String> fragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
+    }
+
+    /*private void showUserInfo() {
         User user = (User) getIntent().getSerializableExtra(SplashActivity.EXTRA_USER);
         String info = "Token user: " + user.getTokenUser() + "\n"
                 + "Token: " + user.getToken() + "\n"
@@ -44,5 +100,5 @@ public class HomeActivity extends AppCompatActivity {
                     + device.getChipID() + "\n\n";
         }
         textContent.setText(info);
-    }
+    }*/
 }
